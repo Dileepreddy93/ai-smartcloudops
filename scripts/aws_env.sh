3546d1839da383ffef22b6782a6910bdc1dbcf1f
dileepreddy93 <dileepkumarreddy12345@gmail.com>
2025-08-17 08:03:49 +0530

    Chore: sync latest files and docs

---
+++ scripts/aws_env.sh
@@ -0,0 +1,7 @@
+export REGION=us-east-1
+export AMI_ID=ami-021589336d307b577
+export KEY_NAME=your-key
+export SUBNET_ID=subnet-08f4eba22a3921965
+export SG_APP=sg-0bd3ea8430aafcb07
+export SG_MON=sg-07c6a35538da3e821
+export ADMIN_IP=2409:408c:1200:a67b:3d30:a2b2:8304:3707
